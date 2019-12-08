284c2b59c36ecaa49187760273a159b742e3aebd
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2019-12-08 12:38:05 +0500

    Modularized 'fortune cookie' functionality into fortune.js

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,12 +1,5 @@
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you do not know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple."
-];
-
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -25,9 +18,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-  var randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', {fortune: randomFortune});
+  res.render('about', {fortune: fortune.getFortune()});
 });
 
 // custom 404 page
